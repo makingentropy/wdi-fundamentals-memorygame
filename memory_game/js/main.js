2255c3fe321281ef8443514d21df0d5cee4b7855
Eric Chance <makingentropy@gmail.com>
2017-06-22 18:51:56 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+//main.js
+//ctrl+shift+j to open console in chrome, win7
+console.log("Up and running!");
+var cardOne="queen";
+var cardTwo="queen";
+var cardThree="king";
+var cardFour="king";
+
+console.log("User flipped " + cardFour);
