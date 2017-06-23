2c03f4a1160e2dd65bfd879cc8df99127fd9a4d6
Eric Chance <makingentropy@gmail.com>
2017-06-23 10:41:21 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,17 +2,40 @@
 //ctrl+shift+j to open console in chrome, win7
 //console.log("Up and running!");
 
-var cards=["queen", "queen","king","king"];
+var cards=[
+	{
+		rank: "queen",
+		suit:"hearts",
+		cardImage:"images/queen-of-hearts.png"
+	},
+	{
+		rank:"queen",
+		suit:"diamonds",
+		cardImage:"images/queen-of-diamonds.png"
+	},
+	{
+		rank:"king",
+		suit:"hearts",
+		cardImage:	"images/king-of-hearts.png"
+	},
+	{
+		rank:"king",
+		suit:"diamonds",
+		cardImage:"images/king-of-diamonds.png"
+	}
+	];
 var cardsInPlay=[];
 var checkForMatch=function(){
 	if(cardsInPlay[0]===cardsInPlay[1]){
+			//alert("Sorry, try again.");
 			console.log("You found a match!");
 		}
-		else{
+	else{
+			//alert("Sorry, try again.");
 			console.log("Sorry, try again.");
 			//the assignment says move this code here,
 			//but shows it as being a console.log() call
-			//rather than the alert it had been. 
+			//rather than the alert it was previously. 
 			//Therefore,
 			//I changed it before moving it because
 			//it appeared to be an implicit instruction.
@@ -28,8 +51,10 @@ var checkForMatch=function(){
 		}
 }
 var flipCard=function(cardId){
-	console.log("User flipped "+cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped "+cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if(cardsInPlay.length===2){
 		checkForMatch();
 	}
