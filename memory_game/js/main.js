bf3c3fb359ccf09709c58a23404ca70ee6d19d1f
Eric Chance <makingentropy@gmail.com>
2017-06-23 05:38:22 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,30 +1,39 @@
 //main.js
 //ctrl+shift+j to open console in chrome, win7
-console.log("Up and running!");
-//var cardOne="queen";
-//var cardTwo="queen";
-//var cardThree="king";
-//var cardFour="king";
-
-//console.log("User flipped " + cardFour);
+//console.log("Up and running!");
 
 var cards=["queen", "queen","king","king"];
 var cardsInPlay=[];
-var cardOne= cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped "+cardOne);
-
-var cardTwo=cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped "+cardTwo);
-
-if(cardsInPlay.length===2){
+var checkForMatch=function(){
 	if(cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else{
-		alert("Sorry, try again.");
-	}
+			console.log("You found a match!");
+		}
+		else{
+			console.log("Sorry, try again.");
+			//the assignment says move this code here,
+			//but shows it as being a console.log() call
+			//rather than the alert it had been. 
+			//Therefore,
+			//I changed it before moving it because
+			//it appeared to be an implicit instruction.
+			//However,
+			//In the very next section, the Deliverable 
+			//clearly pictures that an alert
+			//is still being used instead.
+			//It is unclear which one is the "correct"
+			//deliverable because the exercise was
+			//not carefully edited.
+			//I am giving the console.log version
+			//because it is less annoying.
+		}
 }
+var flipCard=function(cardId){
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length===2){
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
